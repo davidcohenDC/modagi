dcbd85f1e4744e44ca353c9e87af64ed3967878d
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-11-16 18:22:06 +0100

    Add mysqli examples

---
+++ server.php
@@ -0,0 +1,32 @@
+<?php
+
+    $db_user = "root";
+    $db_password = "";
+    $db_name = "Modagi";
+    $host = "127.0.0.1";
+
+    // apre la connessione
+    $conn = new mysqli($host, $db_user, $db_password);
+
+    // controllo se la connessione è stabilita
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else {
+        echo "Connection successful";
+    }
+
+    // query con i prepared statement (in modo da evitare mysql injection)
+        // imposto la query
+    $query_preparator = $conn->prepare("SELECT * FROM table WHERE columnname = ?");
+        // sostituisco il ? con il valore della variabile (normalmente una input di form)
+    $variabile = "nome_colonna";
+    $query_preparator->bind_param('s', $variabile);
+        // eseguo la query
+    $query_preparator->execute();
+        // chiudo il preparatore
+    $query_preparator->close();
+
+    // chiude la connessione
+    $conn->close();
+?>
