a3b41fe1c255960700e83c9d14105fca9ad16758
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-12-09 18:16:43 +0100

    added bootstrap include

--- templates/base.php
+++ templates/base.php
@@ -2,7 +2,10 @@
 <html lang="it">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--page title-->
     <title><?php echo $templateParams["title"]; ?></title>
+    <!--bootstrap include-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
 </head>
 <body>
