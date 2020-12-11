b07c92041c34ee5821d2a266338588d5f0667625
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-12-11 17:52:55 +0100

    create macro.php

--- cart.php
+++ cart.php
@@ -1,14 +1,14 @@
 <?php
 
+require_once("macro.php");
+require_once("cookie.php");
+
 //TODO check if user is logged, else redirect to login page
 $templateParams["title"] = "Shopping cart";
 $templateParams["main"] = "./templates/cart_page.php";
+$templateParams["cssFileName"] = "cart.css";
 
-
-require_once("cookie.php");
 $cookie = new CookieManager();
-define("CART_COOKIE", "CART_COOKIE");
-
 // la setCookie la deve fare Dev quando nella main page l'utente aggiunge qualcosa al carrello
 $cookie->setCookie(CART_COOKIE, json_encode(["articolo1", "articolo2"]));
 $cart["articleList"] = json_decode($cookie->getCookieValue(CART_COOKIE));
--- server.php
+++ server.php
@@ -1,11 +1,10 @@
 <?php
 
+require_once("macro.php");
 require("./DB/Product.php");
 
 $product = new Product();
 //da inserire gli altri creati...
 
-define("CSS_FILE", "./css/");
-define("JS_FILE", "./js/");
 
 ?>
