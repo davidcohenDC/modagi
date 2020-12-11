abcc38dbce4f176bb64429e8eed4f4bee4b9b05f
David <david.cohen@studio.unibo.it>
2020-12-11 17:24:50 +0100

    Adjust the Connections classes

--- DB/Product.php
+++ DB/Product.php
@@ -7,21 +7,6 @@ class Product extends Table {
         parent::__construct("`Prodotto`");
       }
 
-    public function getAll() {
-        //creo un nuovo array per i prodotti
-        $products = array();
-        //effettuo al query estesa dalla Connection e la metto in result
-        $result = parent::query("SELECT * FROM ". $this->table );
-
-        //inserisco nell'array tutti i gli oggetti
-        while($record = $result->fetch_object()) {
-            array_push($products, $record);
-        }
-
-        //ritorno il risultato
-        $result = $products;
-        return $result;
-    }
 }
 
 ?>
