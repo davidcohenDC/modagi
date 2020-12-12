67bbfc92f780b70d0903edc1d15b808c6b93fbe0
David <david.cohen@studio.unibo.it>
2020-12-12 04:44:59 +0100

    Add Product and Category

--- DB/Product.php
+++ DB/Product.php
@@ -1,11 +1,19 @@
 <?php
 
+require_once("Database.php");
+
 //La classe prodotto estende da table che a sua volta estende da Connection. Abbiamo tutti i getter e setter e implementiamo metodi specifici
-class Product extends Table {
-    public function __construct() {
+class Product extends Database {
+    private $table;
+    public function __construct($table) {
         //richiamo la classe setTable dalla Table
-        parent::__construct("`Prodotto`");
+        parent::__construct();
+        $this->table = $table;
       }
+
+    public function selectAll() {
+      return parent::Select("SELECT * FROM ".$this->table);
+    }
 
 }
 
