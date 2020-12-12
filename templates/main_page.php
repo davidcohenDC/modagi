b5cf69e3da87e912be01c487f9b9b309e4d32c2b
David <david.cohen@studio.unibo.it>
2020-12-12 12:21:06 +0100

    Rename to Database.php

--- templates/main_page.php
+++ templates/main_page.php
@@ -55,7 +55,7 @@
 
       <div class="col-lg-3">
 
-        <h1 class="my-4">Shop Name</h1>
+        <h1 class="my-4">SH</h1>
         <div class="list-group">
         <?php foreach($templateParams["categorie"] as $categoria): ?>
           <a href="index.php?categoria=<?php echo $categoria["idCategoria"] ?>" class="list-group-item"><?php echo $categoria["nomeCategoria"] ?></a>
