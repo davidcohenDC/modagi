a4fff6e67e0aad7befbf433cbb8d87d05b8ce27d
David <david.cohen@studio.unibo.it>
2020-12-12 13:49:47 +0100

    Add src product

--- macro.php
+++ macro.php
@@ -3,6 +3,6 @@
 define("CART_COOKIE", "CART_COOKIE");
 define("CSS_FILE", "./css/");
 define("JS_FILE", "./js/");
-
+define("IMG_DIR", "./upload/");
 
 ?>
--- templates/main_page.php
+++ templates/main_page.php
@@ -99,7 +99,7 @@
         <?php foreach($templateParams["prodotti"] as $prodotto): ?>
           <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <a href="#"><img class="card-img-top" src="<?php echo IMG_DIR."/".$prodotto["nome"].".jpg" ?>" alt=""></a>
               <div class="card-body">
                 <h4 class="card-title">
                   <a href="#"><?php echo $prodotto["nome"] ?></a>
