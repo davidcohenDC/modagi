a01c7ffccc65c62c8fc82a751f8dedd652884e13
David <david.cohen@studio.unibo.it>
2020-12-13 14:56:28 +0100

    Restore shop-homepage.php

--- templates/main_page.php
+++ templates/main_page.php
@@ -28,7 +28,7 @@ $templateParams["prodotti"] = $product->Pagination($offset);
   <title>Shop Homepage</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="<?php echo CSS_FILE; ?>bootstrap/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo BOOTSTRAP_CSS_LINK; ?>" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="<?php echo CSS_FILE; ?>shop-homepage.css" rel="stylesheet">
