25bcb6477d8b65bb2244391545c932a4ac3fbc9a
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-12-13 13:23:34 +0100

    add macro for bootstrap css and js linking

--- macro.php
+++ macro.php
@@ -4,6 +4,9 @@ define("SHOP_NAME", "Modagi");
 define("CART_COOKIE", "CART_COOKIE");
 define("CSS_FILE", "./css/");
 define("JS_FILE", "./js/");
+define("BOOTSTRAP_CSS_LINK", "https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css");
+define("BOOTSTRAP_JS_LINK", "https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js");
+define("JQUERY_LINK", "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js");
 define("IMG_DIR", "./upload/");
 define("PROMOTION_DIR", "./upload/promotion/");
 
