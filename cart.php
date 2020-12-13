ee3b59bacc1e5c6cc72e371ef4f083dbaa8c03db
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-12-13 15:21:35 +0100

    implemented DB class for cart

--- cart.php
+++ cart.php
@@ -2,6 +2,7 @@
 
 require_once("macro.php");
 require_once("cookie.php");
+require_once("utilis/DatabaseCart.php");
 
 //TODO check if user is logged, else redirect to login page
 $templateParams["title"] = "Shopping cart";
@@ -10,8 +11,17 @@ $templateParams["cssFileName"] = "cart/cart.css";
 
 $cookie = new CookieManager();
 // la setCookie la deve fare Dev quando nella main page l'utente aggiunge qualcosa al carrello
-$cookie->setCookie(CART_COOKIE, json_encode(["articolo1", "articolo2"]));
-$cart["articleList"] = json_decode($cookie->getCookieValue(CART_COOKIE));
+$cookie->setCookie(CART_COOKIE, json_encode(["Air Max 1", "Af1 Pixel"]));
+$allArticle = json_decode($cookie->getCookieValue(CART_COOKIE));
+
+$db = new DatabaseCart(DB_SERVER_NAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$cart["articleDetails"] = array();
+
+foreach ($allArticle as $key => $articleName) {
+    array_push($cart["articleDetails"], $db->getArticleDetails($articleName));
+}
+
+$cart["totalPrice"] = $db->getTotalPrice($allArticle);
 
 require("./templates/base.php");
 
---
+++ utilis/DatabaseCart.php
@@ -0,0 +1,47 @@
+<?php
+
+class DatabaseCart{
+    private $db;
+
+    // costruttore
+    public function __construct($servername, $username, $password, $dbname) {
+        $this->db = new mysqli($servername, $username, $password, $dbname);
+
+        // controllo se la connessione è andata a buon fine
+        if($this->db->connect_error) {
+            // die interrompe tutto!!
+            die("Connessione al db fallita");
+        }
+    }
+
+    public function getArticleDetails($articleName) {
+        $stmt = $this->db->prepare("SELECT nome, prezzo, descrizione 
+                                    FROM prodotto WHERE nome = ?");
+        $stmt->bind_param("s", $articleName);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $stmt->close();
+
+        // fetch_all recupera il risultato della query e MYSQLI_ASSOC specifico che voglio
+        // che ritornare un array associativo (dizionario)
+        return $result->fetch_all(MYSQLI_ASSOC)[0];
+    }
+
+    public function getTotalPrice($allArticle = []) {
+        $totalPrice = 0;
+        foreach ($allArticle as $key => $article) {
+            $stmt = $this->db->prepare("SELECT prezzo FROM prodotto WHERE nome = ?");
+            $stmt->bind_param("s", $article);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $stmt->close();
+            $totalPrice = $totalPrice + (double)$result->fetch_all(MYSQLI_ASSOC)[0]["prezzo"];
+        }
+
+        return $totalPrice;
+    }
+
+}
+
+?>
