c1cca6af373968b257e45cf995e61b30b9a5bc30
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-12-16 16:33:19 +0100

    converted to new Database class added isVendor function and registration

--- templates/register-form.php
+++ templates/register-form.php
@@ -50,6 +50,11 @@
             </div>
         </div>
 
+        <?php if (isset($templateParams["error"])) : ?>
+            <p class="fs-3 my-5 col-12 text-center fw-bold text-danger">
+                <?php echo $templateParams["error"]; ?>
+            </p>
+        <?php endif ?>
 
         <div class="row justify-content-md-center justify-content-lg-end">
             <div class="my-3 mx-2 col-md-10 col-lg-8">
--- utilis/functions.php
+++ utilis/functions.php
@@ -8,35 +8,43 @@ function isUserLoggedIn()
     return !empty($_SESSION['username']);
 }
 
+function isUserVendor()
+{
+    return !empty($_SESSION['isVendor']) && $_SESSION['isVendor'] == 'S';
+}
+
 function logOut()
 {
     if (isUserLoggedIn()) {
         unset($_SESSION['username']);
+        unset($_SESSION['isVendor']);
     }
 }
 
-function change_url_parameter($url,$parameter,$parameterValue)
+function change_url_parameter($url, $parameter, $parameterValue)
 {
-    $url=parse_url($url);
-    parse_str($url["query"],$parameters);
+    $url = parse_url($url);
+    parse_str($url["query"], $parameters);
     unset($parameters[$parameter]);
-    $parameters[$parameter]=$parameterValue;
-    return  $url["path"]."?".http_build_query($parameters);
+    $parameters[$parameter] = $parameterValue;
+    return  $url["path"] . "?" . http_build_query($parameters);
 }
 
-function addURLParameter($url, $paramName, $paramValue) {
+function addURLParameter($url, $paramName, $paramValue)
+{
     $url_data = parse_url($url);
-    if(!isset($url_data["query"]))
-        $url_data["query"]="";
+    if (!isset($url_data["query"]))
+        $url_data["query"] = "";
 
     $params = array();
     parse_str($url_data['query'], $params);
-    $params[$paramName] = $paramValue;   
+    $params[$paramName] = $paramValue;
     $url_data['query'] = http_build_query($params);
     return build_url($url_data);
 }
 
-function removeqsvar($url, $varname) {
+function removeqsvar($url, $varname)
+{
     list($urlpart, $qspart) = array_pad(explode('?', $url), 2, '');
     parse_str($qspart, $qsvars);
     unset($qsvars[$varname]);
@@ -45,16 +53,16 @@ function removeqsvar($url, $varname) {
 }
 
 
-function build_url($url_data) {
-    $url="";
-    if(isset($url_data['host']))
-    {
+function build_url($url_data)
+{
+    $url = "";
+    if (isset($url_data['host'])) {
         $url .= $url_data['scheme'] . '://';
         if (isset($url_data['user'])) {
             $url .= $url_data['user'];
-                if (isset($url_data['pass'])) {
-                    $url .= ':' . $url_data['pass'];
-                }
+            if (isset($url_data['pass'])) {
+                $url .= ':' . $url_data['pass'];
+            }
             $url .= '@';
         }
         $url .= $url_data['host'];
@@ -72,47 +80,45 @@ function build_url($url_data) {
     return $url;
 }
 
-function filterToQuery($selection) {
+function filterToQuery($selection)
+{
     $filter = "";
     $count = 0;
 
-    if($_SERVER["REQUEST_URI"] == "/") {
+    if ($_SERVER["REQUEST_URI"] == "/") {
         return $selection;
     } else {
-        $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         $query = parse_url($url);
 
-        if(!isset($query["query"])) {
+        if (!isset($query["query"])) {
             return $selection;
         }
 
         $url = $query["query"];
-        parse_str($url,$result);
-        
-        foreach($result as $name=>$value) {
-            if($name == "pag") {
+        parse_str($url, $result);
+
+        foreach ($result as $name => $value) {
+            if ($name == "pag") {
                 //nothing
-            } else if($name == "genere" && $value == 3) {
-                if($count >= 1) {
-                    $filter=  $filter." AND id".$name." IN (1,2,3)";
+            } else if ($name == "genere" && $value == 3) {
+                if ($count >= 1) {
+                    $filter =  $filter . " AND id" . $name . " IN (1,2,3)";
                 } else {
-                    $filter = $filter." WHERE id".$name." IN (1,2,3)";
+                    $filter = $filter . " WHERE id" . $name . " IN (1,2,3)";
                 }
-    
-                $count++;             
+
+                $count++;
             } else {
-                if($count >= 1) {
-                    $filter=  $filter." AND id".$name." = ".$value;
+                if ($count >= 1) {
+                    $filter =  $filter . " AND id" . $name . " = " . $value;
                 } else {
-                    $filter = $filter." WHERE id".$name." = ".$value;
+                    $filter = $filter . " WHERE id" . $name . " = " . $value;
                 }
                 $count++;
             }
         }
-        $selection = $selection.$filter;
+        $selection = $selection . $filter;
         return $selection;
     }
-    
-
-
 }
