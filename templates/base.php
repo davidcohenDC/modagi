2a4514a8fa09b2c7f4b05b392c6b93710c0b4d49
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-12-17 19:58:40 +0100

    added icon change for logged and not logget users in navbar

--- templates/base.php
+++ templates/base.php
@@ -61,7 +61,19 @@
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="user-page.php">Log-in</a>
+                    <a class="nav-link" href="user-page.php">
+                        <?php if (isUserLoggedIn()) : ?>
+                            <?php echo $_SESSION["username"] . " "; ?>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9.854-2.854a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+                            </svg>
+                        <?php else : ?>
+                            <?php echo "Log-in "; ?>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                            </svg>
+                        <?php endif ?>
+                    </a>
                 </li>
             </ul>
             <form class="form-inline my-2 my-md-0">
