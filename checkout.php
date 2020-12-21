32256d8b1abcc34c3bb858a78f13f7d239dba38a
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-12-21 16:08:35 +0100

    init checkout page

---
+++ checkout.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("macro.php");
+require_once("./utilis/CartManager.php");
+require_once("./utilis/functions.php");
+
+$templateParams["title"] = "Checkout";
+$templateParams["cssFileName"] = "checkout/checkout.css";
+
+
+// nel caso in cui è loggato usi un form, nell'altro caso un altro
+if(!isUserLoggedIn()) {
+    $templateParams["main"] = "./templates/checkout_page.php";
+    
+    $cartManager = new CartManager();
+    $cart["articleDetails"] = $cartManager->getArticleDetails();
+    $cart["totalPrice"] = $cartManager->getTotalPrice();
+    
+    // crea array con dati personali dalla session
+}
+else {
+    $templateParams["main"] = "./templates/go_to_login_page.php";
+}
+
+require("./templates/base.php");
+
+?>
