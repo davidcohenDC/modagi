be9ae7292e0dfb2e1bec589331f6707be2215667
David <david.cohen@studio.unibo.it>
2020-12-27 21:53:46 +0100

    correct id occurence

--- product.php
+++ product.php
@@ -3,12 +3,13 @@
 require_once("./server.php");
 require_once("utilis/cartManager.php");
 
-$cartManager = new CartManager();
-
-if(isUserLoggedIn() && isset($_GET["modal"])) {
-    $cartManager->addProduct($_GET["prodotto"],$_GET["taglia"],$_GET["quantita"]);
-    $cartManager->saveCart();
+if(isset($_GET["prodotto"])) {
+    $idprodotto = $_GET["prodotto"];
+} else {
+    $idprodotto = 1;
 }
+
+$cartManager = new CartManager();
 
 $templateParams["main"] = "product-page.php";
 $templateParams["prodotto"] = $product->selectByIdWithQuantity($idprodotto)[0];
@@ -19,6 +20,13 @@ $templateParams["idGenere"] = $product->selectGenereByID($idprodotto)[0];
 $templateParams["idColore"] = $product->selectColoreByID($idprodotto)[0];
 $templateParams["idMarca"] = $product->selectMarcaByID($idprodotto)[0];
 $templateParams["quantitaTaglia"] = $product->selectQuantitaWithNSize($idprodotto);
+
+
+if(isUserLoggedIn() && isset($_GET["modal"])) {
+    $cartManager->addProduct($_GET["prodotto"],$_GET["taglia"],$_GET["quantita"]);
+    $cartManager->saveCart();
+}
+
 
 require_once('templates/base.php');
 require_once("templates/product-page.php");
