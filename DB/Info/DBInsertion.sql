e3c12708bddec58b72800f905427574b5fe7ac83
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-12-28 21:37:27 +0100

    add notifica table to db

--- DB/Info/DBInsertion.sql
+++ DB/Info/DBInsertion.sql
@@ -103,6 +103,14 @@ VALUES (
         'Via Test 3'
     );
 # ---------------------------------------------------------------------- #
+# ADD VALUES "Notifica"                                                  #
+# ---------------------------------------------------------------------- #
+INSERT INTO `Notifica` (`nome`, `contenuto`, `email`)
+VALUES ('test', 'contenuto del test', 'test@gmail.com'),
+       ('test', 'contenuto del test', 'dev@gmail.com'),
+       ('test', 'contenuto del test', 'more@gmail.com');
+
+# ---------------------------------------------------------------------- #
 # ADD VALUES "Categoria"                                                 #
 # ---------------------------------------------------------------------- #
 INSERT INTO `categoria` (`id`, `nome`)
