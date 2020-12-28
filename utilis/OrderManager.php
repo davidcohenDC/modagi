365901d98106d0ae5f63564a7d939bfd226a9295
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-12-28 17:34:23 +0100

    now a order update stock quantity

--- utilis/OrderManager.php
+++ utilis/OrderManager.php
@@ -14,13 +14,32 @@ class OrderManager {
         }
     }
 
-    public function addOrder($userEmail, $productId, $productCount) {
+    public function addOrder($userEmail, $productId, $taglia, $productQuantity) {
         $date = date("Y-m-d");
+        $orderOKid = 1;
 
-        $stmt = $this->db->prepare("INSERT INTO `ordine`(`idProdotto`, `email`, `data`, `quantita`)
-                                    VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $productId, $userEmail, $date, $productCount);
-        return $stmt->execute();;
+        $stmt = $this->db->prepare("INSERT INTO `ordine`(`idProdotto`, `email`, `data`, `quantita`, `idStato`)
+                                    VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $productId, $userEmail, $date, $productQuantity, $orderOKid);
+        if($stmt->execute()) {
+            $stmt = $this->db->prepare("SELECT `id` FROM `taglia` WHERE `numero` = ?");
+            $stmt->bind_param("s", $taglia);
+            if($stmt->execute()) {
+                $idTaglia = $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]["id"];
+
+                $stmt = $this->db->prepare("SELECT `quantita` FROM `prodottitaglie` WHERE `idTaglia` = ? AND `idProdotto` = ?");
+                $stmt->bind_param("ss", $idTaglia, $productId);
+                if($stmt->execute()) {
+                    $currentQuantity = $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]["quantita"];
+                    $finalQuantity = $currentQuantity - $productQuantity;
+
+                    $stmt = $this->db->prepare("UPDATE `prodottitaglie` SET `quantita`= ? WHERE `idTaglia` = ? AND `idProdotto` = ?");
+                    $stmt->bind_param("sss", $finalQuantity, $idTaglia, $productId);
+                    return $stmt->execute();
+                }
+            }
+        }
+        return false;
     }
 }
 
