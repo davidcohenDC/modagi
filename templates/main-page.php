c8fdcdfca8362ea53989cc698f71a59738034ba7
David <david.cohen@studio.unibo.it>
2021-01-05 22:39:25 +0100

    correct home errors

--- templates/main-page.php
+++ templates/main-page.php
@@ -24,12 +24,12 @@ $deleteSelectionHtml = '
     <section id="order-filter">
       <h6>Ordina</h6>
       <div class="list-group">
-        <a href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","ultimi_arrivi")?>">
-        <label class="<?php if(isset($_GET["filter"]) && $_GET["filter"] == "ultimi_arrivi") {echo $selected_css ;}?>">Ultimi Arrivi</label></a>
-        <a href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","prezzo_crescente")?>">
-        <label class="<?php if(isset($_GET["filter"]) && $_GET["filter"] == "prezzo_decrescente") {echo $selected_css ;}?>">Prezzo Crescente</label></a>
-        <a href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","prezzo_decrescente")?>">
-        <label class="<?php if(isset($_GET["filter"]) && $_GET["filter"] == "prezzo_decrescente") {echo $selected_css ;}?>">Prezzo Decrescente</label></a>
+        <a href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","ultimi_arrivi") ?>">
+        <span <?php if(isset($_GET["filter"]) && $_GET["filter"] == "ultimi_arrivi") {echo 'class="'.$selected_css.'"';}?>>Ultimi Arrivi</span></a>
+        <a href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","prezzo_crescente") ?>">
+        <span <?php if(isset($_GET["filter"]) && $_GET["filter"] == "prezzo_crescente") {echo 'class="'.$selected_css.'"';}?>>Prezzo Crescente</span></a>
+        <a href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","prezzo_decrescente") ?>">
+        <span <?php if(isset($_GET["filter"]) && $_GET["filter"] == "prezzo_decrescente") {echo 'class="'.$selected_css.'"' ;}?>>Prezzo Decrescente</span></a>
       </div>
     </section>
     <!-- Section: Order-Filter -->
@@ -38,17 +38,17 @@ $deleteSelectionHtml = '
     <section id="price-filter">
       <h6>Prezzo</h6>
       <div id="range-wrapper">
-        <span for="multi">€0</span>
+        <span>€0</span>
         <form class="multi-range-form">
-          <input id="multi" class="multi-range" type="range" min="1" max="500" step="10" />
+          <input id="multi" class="multi-range" type="range" min="1" max="500" step="10" value="range"/>
         </form>
-        <span for="multi">€500</span>
+        <span>€500</span>
       </div>
     </section>
     <!-- Section: Price-Filter -->
 
 
-    <section id="tables-filter">
+    <div id="tables-filter">
       <?php foreach($tabelle as $tabella): ?>
       <section class="list-<?php echo $tabella ?>">
         <a class="link-table" data-toggle="collapse" href="#collapse_<?php echo $tabella ?>">
@@ -56,20 +56,22 @@ $deleteSelectionHtml = '
           <span class="fa fa-arrow-<?php if(isset($_GET[$tabella])) {echo "down";} else {echo "right";} ?>" aria-hidden="true"></span>
           </h6>
         </a>
+        <div id="collapse_<?php echo $tabella ?>" class="collapse <?php if(isset($_GET[$tabella])) {echo "show";} ?>" >
           <?php foreach($templateParams[$tabella] as $value): ?>
-          <div id="collapse_<?php echo $tabella ?>" class="custom-control collapse <?php if(isset($_GET[$tabella])) {echo "show";} ?>">
-            <input type="radio" class="custom-control-input"
-              id="<?php if($tabella == "taglia") {echo $value["numero"];} else {echo $value["nome"];} ?>"
-              name="check<?php echo ucfirst($tabella) ?>" value="<?php echo $value["id"] ?>"
-              <?php if(isset($_GET[$tabella]) && $_GET[$tabella] == $value["id"]){echo "checked";}?>>
-            <label
-              class="<?php if(isset($_GET[$tabella])) {if($_GET[$tabella] == $value["id"]) {echo $selected_css;}} ?> custom-control-label"
-              for="<?php if($tabella == "taglia") {echo $value["numero"];} else {echo $value["nome"];} ?>"><?php if($tabella == "taglia") {echo $value["numero"];} else {echo $value["nome"];} ?></label>
-          </div>
+            <div class="custom-control">
+              <input type="radio" class="custom-control-input"
+                id="<?php echo $tabella."_".$value["id"]; ?>"
+                name="check<?php echo ucfirst($tabella) ?>" value="<?php echo $value["id"] ?>"
+                <?php if(isset($_GET[$tabella]) && $_GET[$tabella] == $value["id"]){echo "checked";}?> />
+              <label
+                class="<?php if(isset($_GET[$tabella])) {if($_GET[$tabella] == $value["id"]) {echo $selected_css;}} ?> custom-control-label"
+                for="<?php echo $tabella."_".$value["id"]; ?>"><?php if($tabella == "taglia") {echo $value["numero"];} else {echo $value["nome"];} ?></label>
+            </div>
           <?php endforeach ?>
+        </div>
       </section>
       <?php endforeach ?>
-    </section>
+      </div>
     <!-- Section: Tables-Filter -->
 
     <!-- Section: Delete-Filter -->
@@ -89,7 +91,7 @@ $deleteSelectionHtml = '
         rotation stops on keyboard focus on carousel tab controls or hovering the mouse pointer over images.</p>
         <div class="carousel-inner">
           <?php foreach($templateParams["Promo"] as $index => $promo):
-          $prodotto = $product->selectByName($promo)[0]; ?>
+          $prodotto = $product->selectById($promo)[0]; ?>
           <div class="carousel-item <?php if ($index == 0) { echo "active";} ?>">
             <img class="d-block img-fluid" src="<?php echo PROMOTION_DIR."promo_".$promo.".jpg"; ?>" alt="Promozione <?php echo $prodotto["nome"] ?>">
               <div class="carousel-caption text-center">
@@ -116,14 +118,14 @@ $deleteSelectionHtml = '
         </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
           <a class="dropdown-item"
-            href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","ultimi_arrivi")?>"><label
-              data-value="ultimiArrivi" class="dropdown-item">Ultimi Arrivi</label></a>
+            href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","ultimi_arrivi")?>"><span
+              data-value="ultimiArrivi" class="dropdown-item">Ultimi Arrivi</span></a>
           <a class="dropdown-item"
-            href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","prezzo_crescente")?>"><label
-              data-value="prezzoCrescente" class="dropdown-item">Prezzo Crescente</label></a>
+            href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","prezzo_crescente")?>"><span
+              data-value="prezzoCrescente" class="dropdown-item">Prezzo Crescente</span></a>
           <a class="dropdown-item"
-            href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","prezzo_decrescente")?>"><label
-              data-value="prezzoDecrescente" class="dropdown-item">Prezzo Decrescente</label></a>
+            href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], "filter","prezzo_decrescente")?>"><span
+              data-value="prezzoDecrescente" class="dropdown-item">Prezzo Decrescente</span></a>
         </div>
       </div>
 
@@ -132,16 +134,16 @@ $deleteSelectionHtml = '
       <div class="dropdown ml-1 mr-1">
         <button
           class="btn-filter btn btn-light dropdown-toggle mb-4 ml-2 <?php if(isset($_GET[$str_tabella])) { echo "active font-weight-bold text-primary";}?>"
-          type="button" name="check<?php echo $str_tabella ?>" id="dropdownMenuButton" data-toggle="dropdown"
+          type="button" name="check<?php echo $str_tabella ?>" id="dropdownMenu<?php echo $str_tabella ?>" data-toggle="dropdown"
           aria-haspopup="true"
           aria-expanded="false"><?php if(isset($_GET[$str_tabella])) { if($str_tabella == "taglia") {echo strtoupper($templateParams[$str_tabella][$_GET[$str_tabella]-1]["numero"]);} else 
           {echo strtoupper($templateParams[$str_tabella][$_GET[$str_tabella]-1]["nome"]);}} else { echo ucfirst($str_tabella);};?></button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <div class="dropdown-menu" aria-labelledby="dropdownMenu<?php echo $str_tabella ?>">
           <?php foreach($templateParams[$str_tabella] as $value): ?>
           <a class="dropdown-item"
-            href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], $str_tabella,$value["id"])?>"><label
+            href="<?php echo addURLParameters($_SERVER["REQUEST_URI"], $str_tabella,$value["id"])?>"><span
               data-value="<?php echo $value["id"] ?>"
-              class="dropdown-item"><?php if($str_tabella == "taglia") {echo $value["numero"];} else {echo $value["nome"];} ?></label></a>
+              class="dropdown-item"><?php if($str_tabella == "taglia") {echo $value["numero"];} else {echo $value["nome"];} ?></span></a>
           <?php endforeach ?>
         </div>
       </div>
@@ -163,9 +165,9 @@ $deleteSelectionHtml = '
       <div class="products col-6 col-lg-4 col-md-6 mb-4">
           <div class="card h-100">
             <a href="product.php<?php echo "?prodotto=".$prodotto["id"]; ?>">
-              <img class="card-img-top <?php  if($prodotto["stock"] == 0) echo 'not-available'?>" src="<?php echo IMG_DIR."/".$prodotto["nome"].".jpg"; ?>" alt="Scarpa <?php echo $prodotto["nome"]?>">
+              <img class="card-img-top <?php  if($prodotto["stock"] == 0) echo 'not-available'?>" src="<?php echo IMG_DIR."/".$prodotto["id"].".jpg"; ?>" alt="Scarpa <?php echo $prodotto["nome"]?>">
               <?php if($prodotto["stock"] == 0) echo '<span class="product-badge badge-dark" aria-label="non disponibile">ESAURITO</span>'?>
-              <?php if(checkValueInArray($templateParams["migliori"],"idProdotto",$prodotto['id'])) echo '<span class="product-badge badge-warning" aria-label="consigliato">CONSIGLIATO</span>'?>
+              <?php if(checkValueInArray($templateParams["migliori"],"idProdotto",$prodotto['id'])) echo '<span class="product-badge badge-warning">CONSIGLIATO</span>'?>
             </a>
             <div class="card-body">
               <h4 class="card-title">
