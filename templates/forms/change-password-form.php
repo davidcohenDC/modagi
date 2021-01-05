55af1be5b8e0916ccf6056f8743bdfb6ebcc6bbe
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2021-01-05 14:23:52 +0100

    checked modify password

--- templates/forms/change-password-form.php
+++ templates/forms/change-password-form.php
@@ -1,5 +1,5 @@
 <div class="my-3 mx-2 ">
-    <label for="old-password-group" class="form-label fw-bold"> Vecchia Password </label>
+    <label for="old-password" class="form-label fw-bold"> Vecchia Password </label>
     <div id="old-password-group" class="input-group mb-3">
         <input id="old-password" name="old" class="form-control fst-italic password" type="password" placeholder="inserire la password da cambiare" minlength="6" maxlength="20" required />
         <button type="button" id="old-eye" class="eye input-embedded">
@@ -12,7 +12,7 @@
 </div>
 
 <div class="my-3 mx-2 ">
-    <label for="new-password-group" class="form-label fw-bold"> Nuova Password </label>
+    <label for="new-password" class="form-label fw-bold"> Nuova Password </label>
     <div id="new-password-group" class="input-group mb-3">
         <input id="new-password" name="new" class="form-control fst-italic password" type="password" placeholder="inserire la nuova password" minlength="6" maxlength="20" required />
         <button type="button" id="new-eye" class="eye input-embedded">
