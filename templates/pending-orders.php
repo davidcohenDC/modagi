ca2d409cf40137edc4696887580c8ccb2b295852
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2021-01-06 14:36:04 +0100

    fixed section warnings for w3checker

--- templates/pending-orders.php
+++ templates/pending-orders.php
@@ -28,7 +28,7 @@
             <?php $j = 0 ?>
             <!-- date -->
             <?php foreach ($templateParams["dates"] as $date) : ?>
-                <section class="my-3">
+                <div class="my-3">
                     <div class="h3 text-center">
                         <?php echo $date["data"] ?>
                     </div>
@@ -111,7 +111,7 @@
                         </div>
                         <?php $j++; ?>
                     <?php endforeach ?>
-                </section>
+                </div>
                 <hr />
             <?php endforeach ?>
         <?php endif ?>
